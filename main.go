d2c955950dab2bc3fc9f0aace325711483154751
aminshahid573 <createa557733@gmail.com>
2026-05-14 19:03:02 +0545

    fix: fallback to build info for version when not set via ldflags

--- main.go
+++ main.go
@@ -3,11 +3,18 @@ Copyright © 2026 Shahid Amin aminShahid5515@gmail.com
 */
 package main
 
-import "github.com/aminshahid573/gen/cmd"
+import (
+    "runtime/debug"
+    "github.com/aminshahid573/gen/cmd"
+)
 
-// version is set by goreleaser during build
 var version = "dev"
 
 func main() {
-	cmd.Execute(version)
+    if version == "dev" {
+        if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" {
+            version = info.Main.Version
+        }
+    }
+    cmd.Execute(version)
 }
